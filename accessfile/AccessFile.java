285613244a349d8d1dc092778998918cc82f7148
Efraindc <efraindicar@gmail.com>
2023-11-21 22:37:01 -0500

    trabajofinal

---
+++ accessfile/AccessFile.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.accessfile;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class AccessFile {
+
+    public static void main(String[] args) {
+        Principal Principal; /* Define la ventana
+        principal */
+        Principal= new Principal(); /* Crea la ventana
+        principal */
+        Principal.setVisible(true); /* Establece la ventana
+        como visible */
+    }
+}
